4056dacb06361ae668e2a3b5e98261d757c133c1
bfahrenfort <bfahrenfort1@gmail.com>
2024-02-27 11:29:38 -0600

    feat: Listable trait for query expansion

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -20,3 +20,26 @@ pub fn field_count(input: TokenStream) -> TokenStream {
     // Return output tokenstream
     TokenStream::from(output)
 }
+
+#[proc_macro_derive(Listable)]
+pub fn list_fields(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemStruct);
+    let fields_list = input
+        .fields
+        .iter()
+        .map(|e| e.ident.as_ref().unwrap().to_string().to_owned()) // If you use unnamed fields in Entry that's on you
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    let name = &input.ident;
+    let output = quote! {
+        impl #name {
+            pub fn list_fields<'q>() -> &'q str {
+                #fields_list
+            }
+        }
+    };
+
+    // Return output tokenstream
+    TokenStream::from(output)
+}
